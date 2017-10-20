552f1519ae07a98401f936484ad3f2e2d5ad5b45
Jackie Liu <ljmlionshy@live.com>
2017-10-20 17:29:37 +0800

    train the skill for mobile web.

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -2,11 +2,20 @@ let gulp=require('gulp');
 let browserSync=require('browser-sync').create();
 
 gulp.task("browserSync",function(){
-    let hjc=['*.js','*.html','*.css'];
-    // browserSync.init(hjc,{proxy:'localhost:8080'});
-    browserSync.init(hjc,{server:
+    // let hjc=['./deviceOrientation-demo/json/*.js','./deviceOrientation-demo/json/*.html','./deviceOrientation-demo/json/*.css'];
+    browserSync.init({
+    // serveStatic:['.'],
+    proxy:'localhost:8080',
+    ui:{
+        port:9090,
+        weinre:{
+            port:9000,
+        }
+    }
+});
+    // browserSync.init(hjc,{server:
         // {baseDir:
-            './deviceOrientation-demo/json'
+            // './deviceOrientation-demo/json'
         // }
-    })
+    // })
 })
