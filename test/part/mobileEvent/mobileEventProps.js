e27bfd8ea9f8fc171663dad586045acaa2f63748
Jackie Liu <ljmlionshy@live.com>
2017-10-24 17:00:58 +0800

    add new mobileEventProps,optimize the gulp-browserSync flow.

---
+++ test/part/mobileEvent/mobileEventProps.js
@@ -0,0 +1,21 @@
+function js_getDPI() {
+    var arrDPI = new Array;
+    if (window.screen.deviceXDPI) {
+        arrDPI[0] = window.screen.deviceXDPI;
+        arrDPI[1] = window.screen.deviceYDPI;
+    }
+    else {
+        var tmpNode = document.createElement("DIV");
+        tmpNode.style.cssText = "width:1in;height:1in;position:absolute;left:0px;top:0px;z-index:99;visibility:hidden";
+        document.body.appendChild(tmpNode);
+        arrDPI[0] = parseInt(tmpNode.offsetWidth);
+        arrDPI[1] = parseInt(tmpNode.offsetHeight);
+        tmpNode.parentNode.removeChild(tmpNode);    
+    }
+    return arrDPI;
+}
+window.onload=function(){
+		alert("当前屏幕PPI "+js_getDPI());
+}
+
+window.devicePixelRatio;
