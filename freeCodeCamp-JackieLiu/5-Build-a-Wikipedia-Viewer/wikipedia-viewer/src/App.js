f54a3fc39e5d34b0938bab1812b54ef3079b5958
Jackie Liu <ljmlionshy@live.com>
2018-01-31 16:30:48 +0800

    freecode-5 edited,myreads updated.

--- freeCodeCamp-JackieLiu/5-Build-a-Wikipedia-Viewer/wikipedia-viewer/src/App.js
+++ freeCodeCamp-JackieLiu/5-Build-a-Wikipedia-Viewer/wikipedia-viewer/src/App.js
@@ -1,24 +1,41 @@
 import React, { Component } from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'velocity-animate';
+import 'velocity-animate/velocity-ui';
+import { VelocityComponent } from 'velocity-react';
+import fontawesome from '@fortawesome/fontawesome';
+import FontAwesomeIcon from '@fortawesome/react-fontawesome';
 // import logo from './logo.svg';
 // import './App.css';
-const WikiItem=()=>
-  <div></div>
+const WikiItem = () =>
+    <div></div>
 
 
-const WikiList=()=>
-  <div className="mx-5" style={{background:"black",height:500}}>React</div>
+const WikiList = () =>
+    <div className="mx-5" style={{background:"black",height:500}}>React</div>
 
-
+class SeachBar extends Compoent {
+    render() {
+        return (
+        <div>
+			<input type="search" />
+			<FontAwesomeIcon icon="search"/>
+		</div>
+        );
+    }
+}
 class App extends Component {
-  componentDidMount() {
-    
-  }
-  render() {
-    return (
-      <WikiList/>
-    );
-  }
+    componentDidMount() {
+
+    }
+    render() {
+        return (
+            <SeachBar/> 
+            <VelocityComponent >
+            	<WikiList/> 
+            </VelocityComponent>
+        );
+    }
 }
 
 export default App;
