cef3a41a335eef36159d750906bdbfe6aa591afb
Jackie Liu <ljmlionshy@live.com>
2018-03-02 16:29:38 +0800

    freecodecamp 6th project

--- freeCodeCamp-JackieLiu/6-Use-the-Twitchtv-JSON-API/api.bundle.js
+++ freeCodeCamp-JackieLiu/6-Use-the-Twitchtv-JSON-API/api.bundle.js
@@ -1,4 +1,5 @@
 (function(){function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s}return e})()({1:[function(require,module,exports){
+// 在服务端配置dom环境
 // var jsdom = require("jsdom");
 // var {JSDOM}=jsdom;
 // // ---Deprecated
@@ -18,14 +19,71 @@ fetch("https://wind-bow.gomix.me/twitch-api/streams/react?callback=?", { method:
 // 2- 在服务端没有打印出jquery ajax方法，可以打印出数据，但是ajax缺少了异步流处理的优越，也需要在url加上?callback=?的后缀才能使用jsonp
 $.getJSON("https://wind-bow.gomix.me/twitch-api/streams/Dyrus?callback=?", function(data) { console.log(data); })
 // 3- 在服务端无法用fetchJsonp,即使引用了node-fetch；fetch的response不能缩写成res,会引发错误；优点使用流式处理，缺点是生成的代码量较多，因为把模块代码引了进去；该模块源码没有使用fetch
-fetchJsonp("https://wind-bow.gomix.me/twitch-api/streams/Dyrus", { jsonpCallback: 'callback' }).then(response=> response.json()
-).then(function(json) {
+fetchJsonp("https://wind-bow.gomix.me/twitch-api/channels/react", { jsonpCallback: 'callback' }).then(response => response.json()).then(function(json) {
+    console.log(json)
+}).catch(function(error) {
+    console.log(error)
+})
+// 4- 比较以上数据，显示来自users的数据
+fetchJsonp("https://wind-bow.gomix.me/twitch-api/users/react", { jsonpCallback: 'callback' }).then(response => response.json()).then(function(json) {
     console.log(json)
 }).catch(function(error) {
     console.log(error)
 })
 
+//状态切换显示，同时更改状态数据ui组件
+$(document).ready(function() {
+    $(".selector").click(function() {
+        $(".selector").removeClass("active");
+        $(this).addClass("active");
+        var status = $(this).attr('id');
+        if (status === "all") {
+            $(".online, .offline").removeClass("hidden");
+        } else if (status === "online") {
+            $(".online").removeClass("hidden");
+            $(".offline").addClass("hidden");
+        } else {
+            $(".offline").removeClass("hidden");
+            $(".online").addClass("hidden");
+        }
+    })
+});
 //项目采用第3种选择
+
+//---------6-project----------//
+const wikiUsers = ['AdmiralBulldog', 'Amaz', 'Aphromoo', 'Pavel Beltiukov', 'Biofrost', 'J. T. Brown', 'Tom Cassell', 'Christoffer Stub', 'Stanislav Cifka', 'Dyrus',
+    'Faker',
+    'Forsen',
+    'Dan Gheesling',
+    'Kevin Godec',
+    'Grubby',
+    'Kat Gunn',
+    'H3h3Productions',
+    'Hafu',
+    'Huni',
+    'Ice Poseidon',
+    'Mia Khalifa',
+    'Brian Kibler',
+    'Kolento',
+    'Kripparrian',
+    'Maikelele',
+    'Joseph Marquez',
+    'Andy Milonakis',
+    'N0thing',
+    'Sean Plott',
+    'Rekkles',
+    'SethBling',
+    'Sneaky',
+    'Bjergsen',
+    'Stixxay',
+    'T-Pain',
+    'Trump',
+    'Meg Turney'
+]
+
+const url="https://wind-bow.gomix.me/twitch-api/channels/";
+
+// const dataUsers=wikiUsers.map(dataUser=>(fetchJsonp(url+user).then(response=>response.json()).then(user=>))
 },{"fetch-jsonp":2}],2:[function(require,module,exports){
 (function (global, factory) {
   if (typeof define === 'function' && define.amd) {
