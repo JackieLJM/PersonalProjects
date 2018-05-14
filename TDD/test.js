0c9b04d517198839406828f07ce99584fe26d6ac
Jackie Liu <ljmlionshy@live.com>
2018-05-14 14:18:27 +0800

    test

--- TDD/test.js
+++ TDD/test.js
@@ -143,3 +143,25 @@
 // console.log(btrueString);
 
 
+class Math {
+  @log
+  add(a, b) {
+    return a + b;
+  }
+}
+
+function log(target, name, descriptor) {
+  var oldValue = descriptor.value;
+
+  descriptor.value = function() {
+    console.log(`Calling ${name} with`, arguments);
+    return oldValue.apply(this, arguments);
+  };
+
+  return descriptor;
+}
+
+const math = new Math();
+
+// passed parameters should get logged now
+math.add(2, 4);
